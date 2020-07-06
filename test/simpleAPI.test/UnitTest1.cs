797f25908ef4b5c1a6927a5267f4670ced63dd90
Ben Hill <ben.hill@kneat.org>
2020-07-06 14:26:58 +0100

    Intial commit

---
+++ test/simpleAPI.test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+
+namespace simpleAPI.test
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
